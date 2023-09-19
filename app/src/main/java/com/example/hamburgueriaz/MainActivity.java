fc178cf8d2b528624c3ff69d96ce3557c48f1ff1
Taisa Braga <taisabraga.adm@gmail.com>
2023-09-19 14:54:00 -0300

    feat: create add and reduce value function

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.hamburgueriaz;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private TextView amountValueTextView;
+    private Button addButton;
+    private Button subtractButton;
+    private int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountValueTextView = findViewById(R.id.amountValue);
+        addButton = findViewById(R.id.addValue);
+        subtractButton = findViewById(R.id.reduceValue);
+
+        // Configurar os ouvintes de clique para os botões
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementQuantity();
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                decrementQuantity();
+            }
+        });
+    }
+
+    // Função para incrementar a quantidade
+    private void incrementQuantity() {
+        if (quantity >= 0) {
+            quantity++;
+            updateQuantityTextView();
+        }
+    }
+
+    // Função para decrementar a quantidade
+    private void decrementQuantity() {
+        if (quantity > 0) {
+            quantity--;
+            updateQuantityTextView();
+        }
+    }
+
+    // Atualiza o TextView da quantidade com o valor atual
+    private void updateQuantityTextView() {
+        amountValueTextView.setText(String.valueOf(quantity));
     }
 }
